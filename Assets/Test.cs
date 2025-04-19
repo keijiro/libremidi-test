a1d4619586821ae4f5aafcfb8f114bc6e42afb28
Keijiro Takahashi <keijiro@gmail.com>
2025-04-19 14:52:35 +0900

    Test

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -5,9 +5,12 @@ public sealed class Test : MonoBehaviour
 {
     void Start()
     {
-        MidiSystem.QueryMidi1Apis(
-                (System.IntPtr ctx, Api api)
-                  => Debug.Log($"{api} : {MidiSystem.GetApiIdentifier(api)} : {MidiSystem.GetApiDisplayName(api)}"));
         Debug.Log(MidiSystem.GetVersion());
+
+        MidiSystem.QueryMidi1Apis
+          ((System.IntPtr ctx, Api api)
+              => Debug.Log($"{api}" +
+                           $" : {MidiSystem.GetApiIdentifier(api)}" +
+                           $" : {MidiSystem.GetApiDisplayName(api)}"));
     }
 }
